f55ca15612b1fcf3dc212173de442ce6f7235a6b
小易 <76422937@qq.com>
2017-06-14 10:23:41 +0800

    resume 1.0
    My resume on github.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <container></container>
+    </div>
+</template>
+
+<script>
+    import container from './components/container';
+
+    export default {
+        name: 'app',
+        components: {
+            container
+        }
+    };
+</script>
+
+<style lang="scss">
+    html, body {
+        padding: 0;
+        margin: 0;
+        line-height: 1;
+    }
+    @font-face {
+        font-family: yuanti;
+        src: url(/static/yuanti.ttf);
+    }
+    #app {
+        font-family: yuanti;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #333;
+    }
+</style>
---
+++ src/components/container.vue
@@ -0,0 +1,367 @@
+<template>
+    <div class="container">
+        <div class="page">
+            <section class="head">
+                <div class="resume-name">刘通简历</div>
+                <div class="aims">求职目标: PHP开发工程师 / 工资面议 / 大专 / 全职</div>
+            </section>
+
+            <section class="base-info">
+                <div class="tag">基本信息</div>
+                <div class="info">
+                    <div class="row">
+                        <div class="item">姓　　名: 刘通</div>
+                        <div class="item">性　　别: 男</div>
+                        <div class="item">年　　龄: 26</div>
+                    </div>
+                    <div class="row">
+                        <div class="item">工作经验: 4年</div>
+                        <div class="item">工作状态: 离职</div>
+                        <div class="item">到岗时间: 随时</div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="assess-info">
+                <div class="tag">自我评价</div>
+                <div class="info">
+                    精通PHP语言；熟练使用MySQL；熟练掌握ThinkPHP框架；了解Redis；了解MongoDB；熟练使用Linux，能独立搭建LAMP运行环境并了解Nginx；熟练掌握HTML5+CSS3+JavaScript；熟练掌握Bootstrap框架；熟练掌握VUE2.0框架；熟练使用GIT&amp;SVN版本控制；掌握WEB前端和PHP相关调试工具；对PHP+MySQL在Windows/Linux环境下的开发、调试有深入的认识。有1年团队管理经验，管理7人开发成员。
+                </div>
+            </section>
+
+            <section class="education-info">
+                <div class="tag">教育经历</div>
+                <div class="info">
+                    <div class="row">
+                        <div class="item">2007.09 - 2010.06</div>
+                        <div class="item">院校: 满城区职业技术教育中心</div>
+                        <div class="item">专业: 计算机应用</div>
+                    </div>
+                    <div class="row">
+                        <div class="item">2010.09 - 2013.06</div>
+                        <div class="item">院校: 石家庄职业技术学院</div>
+                        <div class="item">专业: 信息安全技术</div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="jobs-info">
+                <div class="tag">工作经历</div>
+                <div class="info">
+                    <div class="row job">
+                        <div class="item name">那片田互联网科技（北京）有限公司</div>
+                        <div class="item"></div>
+                        <div class="item period">2016.05 - 2017.06</div>
+                    </div>
+                    <div class="row intro">
+                        <div class="item">所在部门: 技术部</div>
+                        <div class="item">所任职位: 技术经理</div>
+                        <div class="item">管理人数: 7人</div>
+                    </div>
+
+                    <div class="row job">
+                        <div class="item name">宇德（北京）投资管理有限公司</div>
+                        <div class="item"></div>
+                        <div class="item period">2015.09 - 2016.05</div>
+                    </div>
+                    <div class="row intro">
+                        <div class="item">所在部门: 技术部</div>
+                        <div class="item">所任职位: PHP开发</div>
+                        <div class="item"></div>
+                    </div>
+
+                    <div class="row job">
+                        <div class="item name">魔力爱普（北京）互联网技术有限公司</div>
+                        <div class="item"></div>
+                        <div class="item period">2014.12 - 2015.09</div>
+                    </div>
+                    <div class="row intro">
+                        <div class="item">所在部门: 技术部</div>
+                        <div class="item">所任职位: PHP开发</div>
+                        <div class="item"></div>
+                    </div>
+
+                    <div class="row job">
+                        <div class="item name">宇德（北京）投资管理有限公司</div>
+                        <div class="item"></div>
+                        <div class="item period">2013.07 - 2014.11</div>
+                    </div>
+                    <div class="row intro">
+                        <div class="item">所在部门: 技术部</div>
+                        <div class="item">所任职位: PHP研发</div>
+                        <div class="item"></div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="project-info">
+                <div class="tag">项目经验</div>
+                <div class="info">
+                    <div class="item">
+                        <div class="name">会通平台（北京宝盈科技互动有限公司）</div>
+                        <div class="trust">项目职责: 平台开发、API开发</div>
+                        <div class="significance">项目意义: 让用户在线创建会议活动类应用，并可自定义APP引导页、启动页、主题风格、功能模块的内容管理系统。用户构建完毕可通过平台管理页面一键打包发布APP，完全不需要任何技术就可以创建自己的原生APP。</div>
+                        <div class="intro"></div>
+                    </div>
+
+                    <div class="item">
+                        <div class="name">魔力平台（魔力爱普（北京）互联网技术有限公司）</div>
+                        <div class="trust">项目职责: 会员模块改版、微商城开发（含支付）、站内信开发</div>
+                        <div class="significance">项目意义: 魔力APP是一个搭建个性化APP和各类公众帐号的超级工具。通过登录该网站，任何不懂技术的用户，仅需要简单的步骤即可随心所欲地创建一个专属的APP或微信公众账号，不必依附外包，也无需繁琐的代码程序。</div>
+                        <div class="intro"></div>
+                    </div>
+
+                    <div class="item">
+                        <div class="name">爱银网（宇德（北京）投资管理有限公司）</div>
+                        <div class="trust">项目职责: API开发</div>
+                        <div class="significance">项目意义: 爱银网（www.aiyinwang.com）是国内领先的互联网金融O2O交易平台，致力于为有资金需求的融资人和有闲散资金的投资者搭建一个诚信、规范、专业、透明的互联网金融理财平台。</div>
+                        <div class="intro"></div>
+                    </div>
+
+                    <div class="item">
+                        <div class="name">那片田（那片田互联网科技（北京）有限公司）</div>
+                        <div class="trust">项目职责: API开发、微商城开发、后台管理开发、团队管理</div>
+                        <div class="significance">项目意义: 那片田（m.therye.cc）是一个倡导有机生活的生鲜网购平台，经营种类包含进口水果、有机蔬菜、海鲜肉禽、五谷杂粮、饮品零食等全品类，那片田始终坚守“如关爱家人一般关爱顾客”的创业初心，为顾客提供“有机，天然，高品质”的生鲜食材。</div>
+                        <div class="intro">
+                            <p>项目介绍: 项目为前后端分离，前端使用AngularJS、Layer等，后端使用ThinkPHP5开发，接口采用RESTFul规范风格，符合现代API开发标准。</p>
+                            <ol>
+                                <li>WAP商城多城市、多点配送，根据定位调取不同库存数据。采用高德地图API开发定位和地图选址功能，集成支付宝支付、微信支付。图片使用懒加载提升用户体验，数据使用Storage作为本地存储。</li>
+                                <li>后台实现订单管理、新订单提醒（websocket）、超时订单自动关闭、对账单下载、商品、库存、会员、优惠券、门店自提点地图数据、商城首页可视化配置、推荐热搜词、短信群发、消息推送、广告位等功能。</li>
+                                <li>门店端实现扫描二维码（基于微信API）或输入订单号、提货码进行快速订单查询，以便店员配货。</li>
+                            </ol>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="name">TEAdmin后台开发框架【个人项目】</div>
+                        <div class="trust"></div>
+                        <div class="significance"></div>
+                        <div class="intro">项目介绍: TEAdmin（admin.teadmin.cc）是基于 ThinkPHP5.0.7 &amp; PixelAdmin2.3.1 开发的基础后台开发框架，具备现代化友好的界面设计、完美的权限控制等基础功能，方便基于框架进行项目开发。目前框架处于不断完善阶段，会有更多基础功加入。</div>
+                    </div>
+
+                    <div class="item next-page">
+                        <div class="name">满城果品【个人项目】</div>
+                        <div class="trust"></div>
+                        <div class="significance"></div>
+                        <div class="intro">项目介绍: 满城果品是基于VUE2开发的生鲜商城，后台基于TEAdmin开发。目前处于开发阶段。</div>
+                    </div>
+
+                    <div class="item">
+                        <div class="name">沧州爱尚装饰【个人项目】</div>
+                        <div class="trust"></div>
+                        <div class="significance"></div>
+                        <div class="intro">项目介绍: 沧州爱尚装饰是基于VUE2开发的VR样板间展示应用，VR使用krpano制作，客户可在线报装，系统自动通过微信消息通知微信客服人员。</div>
+                    </div>
+
+                    <div class="item">
+                        <div class="name">☆ 刘通简历 ☆</div>
+                        <div class="trust"></div>
+                        <div class="significance">项目特点: 本简历是基于HTML开发，方便直接打印或者打印成PDF文件，相比Word更易编辑和排版。</div>
+                        <div class="intro">项目介绍: 本简历使用HTML制作，使用CSS Print @PAGE规则实现A4纸规格。</div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'hello'
+    };
+</script>
+
+<style lang="scss">
+@page {
+    size: A4;
+}
+@media print {
+    .page {
+        page-break-before: always;
+    }
+    .next-page {
+        margin-top: 53px;
+        page-break-before: always;
+    }
+
+    .intro {
+        page-break-inside: auto;
+    }
+}
+
+.container {
+    .page {
+        padding: 53px 66px;
+        margin: 0 auto;
+        width: 794px;
+        height: 1123px;
+        box-sizing: border-box;
+
+        section {
+            margin-top: 20px;
+
+            .tag {
+                position: relative;
+                font-size: 10pt;
+                font-weight: bold;
+                padding-left: 7px;
+
+                &:before {
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    content: '';
+                    display: inline-block;
+                    width: 3px;
+                    height: 10pt;
+                    background-color: #30ce51;
+                }
+            }
+            .info {
+                margin-top: 3px;
+                font-size: 8pt;
+            }
+            // 头部
+            &.head {
+                margin-top: 0;
+
+                .resume-name {
+                    font-size: 14pt;
+                    font-weight: bold;
+                }
+
+                .aims {
+                    margin-top: 8px;
+                    font-size: 10pt;
+                    font-weight: bold;
+                }
+            }
+
+            // 基础信息
+            &.base-info {
+                .info {
+                    > .row {
+                        display: flex;
+                        padding-top: 5px;
+
+                        .item {
+                            flex: 1;
+
+                            &:not(:first-child) {
+                                padding-left: 20%;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // 自我评价
+            &.assess-info {
+                .info {
+                    margin-top: 6px;
+                    text-indent: 2em;
+                    text-align: justify;
+                    line-height: 1.5;
+                }
+            }
+
+            // 教育经历
+            &.education-info {
+                .info {
+                    > .row {
+                        display: flex;
+                        padding-top: 5px;
+
+                        .item {
+                            flex: 1;
+
+                            &:nth-child(2) {
+                                flex: 1.5;
+                                padding-left: 20%;
+                            }
+
+                            &:nth-child(3) {
+                                padding-left: 10%;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // 工作经历
+            &.jobs-info {
+                .info {
+                    > .row.job {
+                        display: flex;
+                        padding-top: 5px;
+
+                        &:not(:first-child) {
+                            margin-top: 6px;
+                            padding-top: 8px;
+                            // border-top: 1px dotted #f2f2f2;
+                            box-sizing: border-box;
+                        }
+
+                        .item {
+                            flex: 1;
+
+                            &:not(:first-child) {
+                                padding-left: 10%;
+                            }
+
+                            &.name {
+                                flex: 2;
+                                font-weight: bold;
+                            }
+                        }
+                    }
+
+                    > .row.intro {
+                        display: flex;
+                        padding-top: 5px;
+
+                        .item {
+                            flex: 1;
+
+                            &:not(:first-child) {
+                                padding-left: 20%;
+                            }
+                        }
+                    }
+                }
+            }
+
+            &.project-info {
+                .info {
+                    margin-top: 6px;
+                    text-align: justify;
+                    line-height: 1.5;
+
+                    .item {
+                        &:not(:first-child) {
+                            padding-top: 5px;
+                        }
+
+                        .name {
+                            font-weight: bold;
+                        }
+
+                        .intro {
+                            p {
+                                padding: 0;
+                                margin: 0;
+                            }
+
+                            ol {
+                                padding: 0;
+                                padding-left: 1em;
+                                margin: 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
